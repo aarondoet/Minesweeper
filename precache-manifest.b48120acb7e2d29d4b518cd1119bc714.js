6b4ec8c71649c547551ac924bdc34a5acd6444a8
Aaron <42084688+l0c4lh057@users.noreply.github.com>
2020-05-03 15:19:33 +0200

    Updates

---
+++ precache-manifest.b48120acb7e2d29d4b518cd1119bc714.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f19a42c946924e9d0198c1d709b53667",
+    "url": "/Minesweeper/index.html"
+  },
+  {
+    "revision": "86ff23b2641005f46561",
+    "url": "/Minesweeper/static/css/main.ba56f0f0.chunk.css"
+  },
+  {
+    "revision": "6831d610ebdbd358c70e",
+    "url": "/Minesweeper/static/js/2.e430ac1d.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/Minesweeper/static/js/2.e430ac1d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "86ff23b2641005f46561",
+    "url": "/Minesweeper/static/js/main.b16d369b.chunk.js"
+  },
+  {
+    "revision": "97fc551654f42fc8b326",
+    "url": "/Minesweeper/static/js/runtime-main.cd5efc21.js"
+  },
+  {
+    "revision": "d18de02dc7ab0f190c873c573230782e",
+    "url": "/Minesweeper/static/media/flag.d18de02d.svg"
+  }
+]);
